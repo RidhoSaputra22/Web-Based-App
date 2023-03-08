2dbf15ab27a10e4d215e30420841c6fcdf0ce811
ridho-code <60309031+ridho-code@users.noreply.github.com>
2023-03-08 20:29:41 +0800

    add slider

--- Psychic Engine/Gravity/main.js
+++ Psychic Engine/Gravity/main.js
@@ -1,64 +1,62 @@
 //createBy: Ridho Saputra
+var canvas = document.getElementById('canvas'),
+ctx = canvas.getContext('2d'),
+width = canvas.width = window.innerWidth,
+height = canvas.height = 350,
+w = width / 2,
+h = height / 2;
 
-window.onload = function(){
-    var canvas = document.getElementById('canvas'),
-        ctx = canvas.getContext('2d'),
-        width = canvas.width = window.innerWidth,
-        height = canvas.height = 350,
-        w = width / 2,
-        h = height / 2;
+var massRange = document.getElementById('massRange')
 
-    var sun = vector.create(w, h),
-        sunMass = 20000;
+var sun = vector.create(w, h),
+sunMass = 20000;
 
-    var planet = vector.create(w + 150, h),
-        planetVelocity = vector.create(0,0);
+var planet = vector.create(w + 150, h),
+planetVelocity = vector.create(0,0);
 
-    var grav = vector.create(0,0);
-        planetVelocity.setLength(10);
-        planetVelocity.setAngle(-Math.PI / 2);
+var grav = vector.create(0,0);
+planetVelocity.setLength(10);
+planetVelocity.setAngle(-Math.PI / 2);
 
+massRange.oninput = function(){
+sunMass = this.value
 
-    // Everything happen here
-    render()
-    function render(){
-        ctx.clearRect(0,0, width, height)
+} 
+ // Everything happen here
+ render()
+ function render(){
+     ctx.clearRect(0,0, width, height)
 
-        //Get the distance between the sun and the planet
-        var dx = sun.getX() - planet.getX(),
-            dy = sun.getY() - planet.getY();
-        var dist = Math.sqrt(dx * dx + dy * dy)
-        
-        //Set the length and angle of grav vector
-        grav.setLength(sunMass / (dist * dist))
-        grav.setAngle(Math.atan2(dy,dx))
-        
-        //Update the planet velocity and planet posisition
-        planetVelocity.addTo(grav)        
-        planet.addTo(planetVelocity)
-        
-        //Create sun
-        ctx.beginPath()
-        ctx.fillStyle = "red";
-        ctx.arc(sun.getX(), sun.getY(), 20, 0 , Math.PI * 2);
-        ctx.fill()
-        ctx.stroke()
+     //Get the distance between the sun and the planet
+     var dx = sun.getX() - planet.getX(),
+         dy = sun.getY() - planet.getY();
+     var dist = Math.sqrt(dx * dx + dy * dy)
+     
+     //Set the length and angle of grav vector
+     grav.setLength(sunMass / (dist * dist))
+     grav.setAngle(Math.atan2(dy,dx))
+     
+     //Update the planet velocity and planet posisition
+     planetVelocity.addTo(grav)        
+     planet.addTo(planetVelocity)
+     
+     //Create sun
+     ctx.beginPath()
+     ctx.fillStyle = "red";
+     ctx.arc(sun.getX(), sun.getY(), 20, 0 , Math.PI * 2);
+     ctx.fill()
+     ctx.stroke()
 
-        //Create Planet
-        ctx.beginPath()
-        ctx.fillStyle = "blue";
-        ctx.arc(planet.getX(), planet.getY(), 10, 0 , Math.PI * 2);
-        ctx.fill()
-        ctx.stroke()
-        
-        //Make Animation
-        requestAnimationFrame(render)
+     //Create Planet
+     ctx.beginPath()
+     ctx.fillStyle = "blue";
+     ctx.arc(planet.getX(), planet.getY(), 10, 0 , Math.PI * 2);
+     ctx.fill()
+     ctx.stroke()
+     
+     //Make Animation
+     requestAnimationFrame(render)
 
-    }
+ }
 
 
-
-
-
-
-}
